63e30a8233428236bf1e97d2cdb8ce5cbb7dbd0d
R. Alex Barbieri <luck@mit.edu>
2015-05-01 11:15:28 -0400

    add Kaya's fixes for threshold finder and rate drawing

--- findthes.C
+++ findthes.C
@@ -27,66 +27,75 @@ double find(TString infname, Double_t pTthes, Double_t effthes, int cent)
 {
   TFile* inf = new TFile(infname);
   int i=0,j=0;
-  Bool_t flag=false;
+  Bool_t L1threshold_FOUND=false;		// "flag" renamed to "L1threshold_FOUND"
   TString ingname;
   for(i=99;i>=0;i-=1)
+  {
+    ingname = Form("asymm_pt_%d_%d",i*2,cent);
+    TGraphAsymmErrors* ga = (TGraphAsymmErrors*)inf->Get(ingname);
+    if(!ga) break;
+    Double_t vx,vy,intermin=1000000.;
+    //////// Kaya's modificiation ////////
+    Double_t vx_selected=-1;
+    Bool_t eff4theRest  =true;	// true if the turn on curve stays 100% for
+    // each offline pt larger than the given threshold
+    //////// Kaya's modificiation - END ////////
+    for(j=0;j<ga->GetN();j++)
     {
-      ingname = Form("asymm_pt_%d_%d",i*2,cent);
-      TGraphAsymmErrors* ga = (TGraphAsymmErrors*)inf->Get(ingname);
-      if(!ga) break;
-      Double_t vx,vy,interx,intermin=1000000.;
-      Bool_t flagx=false;
-      for(j=0;j<ga->GetN();j++)
+      ga->GetPoint(j,vx,vy);
+      if(vx<=pTthes)	 //////// Kaya's modificiation ////////
+      {
+	//cout<<vy<<endl;
+	if(vy>=effthes)
 	{
-	  ga->GetPoint(j,vx,vy);
-	  if(vx==pTthes)
-	    {
-	      //cout<<vy<<endl;
-	      flagx=true;
-	      if(vy>=effthes)
-		{
-		  flag=true;
-		}
-	      break;
-	    }
-	  if(TMath::Abs(vx-pTthes)<=intermin)
-	    {
-	      intermin = TMath::Abs(vx-pTthes);
-	      interx = vx;
-	    }
+	  vx_selected=vx;
+	  eff4theRest=true;
+	  L1threshold_FOUND=true;
 	}
-      if(!flagx)
+	//		  break;		//////// Kaya's modificiation ////////
+      }
+      if(L1threshold_FOUND)	// an L1 threshold for which turn on curve hits 100% eff. not later than "pTthes".
+	// check the efficiency of this L1 threshold for the remaining offline pT as well.
+      {
+	if(vy<effthes)
 	{
-	  cout<<endl;
-	  cout<<">>>> ERROR"<<endl;
-	  cout<<">>>> Graph <"<<ingname<<"> has no point at "<<pTthes<<"GeV"<<endl;
-	  cout<<">>>> The closest point is"<<interx<<endl;;
-	  cout<<">>>> ERROR ENDS"<<endl;
-	  cout<<endl;
-	  return -1;
+	  eff4theRest=false;
+	  L1threshold_FOUND=false;	// we want turn curve to stay at 100% once it hits 100%
 	}
-      if(flag) break;
+      }
+      if(TMath::Abs(vx-pTthes)<=intermin)
+      {
+	intermin = TMath::Abs(vx-pTthes);
+      }
     }
-  if(!flag)
+    if (L1threshold_FOUND)	// an L1 threshold has been found
     {
-      cout<<endl;
-      cout<<">>>> ERROR"<<endl;
-      cout<<"ERROR: File<"<<infname<<"> has no thredshold for "<<effthes*100<<"% at "<<pTthes<<" GeV/c"<<endl;
-      cout<<">>>> ERROR ENDS"<<endl;
-      cout<<endl;
-	  return -1;
+      cout << "An L1 threshold has been found." << endl;
+      cout << "studied offline pT : " << pTthes << endl;
+      cout << "L1 threshold : " << (i*2) << endl;
+      cout << "100% eff. is reached at offline pT : " << vx_selected << endl;
+      cout << "100% eff. for each offline pt larger than the given threshold : " << eff4theRest << endl;
 
-    }
-  else
-    {
-      //cout<<endl;
-      //cout<<">>>> RESULT"<<endl;
-      //cout<<">>>> File <"<<infname<<"> has the thredshold <"<<ingname<<"> for "<<effthes*100<<"% at "<<pTthes<<" GeV/c"<<endl;
-      //cout<<">>>> RESULT ENDS"<<endl;
-      //cout<<endl;
+      // if(!flagx)
+      // {
+      // 	cout<<endl;
+      // 	cout<<">>>> WARNING"<<endl;	//////// Kaya's modificiation ////////
+      // 	cout<<">>>> Graph <"<<ingname<<"> has no point at "<<pTthes<<"GeV"<<endl;
+      // 	cout<<">>>> The closest point is "<<interx<<endl;;
+      // 	cout<<">>>> WARNING ENDS"<<endl;	//////// Kaya's modificiation ////////
+      // 	cout<<endl;
+      // }
+
       return i*2;
     }
-
+  }
+  // none of the L1 thresholds matched
+  cout<<endl;
+  cout<<">>>> ERROR"<<endl;
+  cout<<"ERROR: File<"<<infname<<"> has no thredshold for "<<effthes*100<<"% at "<<pTthes<<" GeV/c"<<endl;
+  cout<<">>>> ERROR ENDS"<<endl;
+  cout<<endl;
+  return -1;
 }
 
 
@@ -130,7 +139,7 @@ void findthes(TString inFileName = "Hydjet502_JetResults_zeroWalls.root",TString
   }
 
   const int Nthresholds=11;
-  double offlinethresholds[Nthresholds]={30.25,40.25,50.25,60.25,70.25,80.25,90.25,100.75,120.75,130.25,140.75};
+  double offlinethresholds[Nthresholds]={30,40,50,60,70,80,90,100,120,130,140};
   double L1thresholds[Nthresholds]={-1.,-1.,-1.,-1.,-1.,-1.,-1.,-1.,-1.,-1.,-1.};
   double rates[Nthresholds]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
 
@@ -138,29 +147,32 @@ void findthes(TString inFileName = "Hydjet502_JetResults_zeroWalls.root",TString
   for(int m=0; m<Nthresholds;m++){
     L1thresholds[m]=find(infn, offlinethresholds[m], 1.,centrality);
     rates[m]=rate->GetBinContent(int(L1thresholds[m]/2)+1)*30000;
-    std::cout<<"offline threshold="<<offlinethresholds[m]<<", L1 threshold="<<L1thresholds[m]<<", rate="<<rates[m]<<std::endl;
+    std::cout<<"offline threshold="<<offlinethresholds[m]<<", L1 threshold="<<L1thresholds[m]<<", rate="<<rates[m]<<std::endl << std::endl;
   }
-   TCanvas* c1 = new TCanvas("c1","A Simple Graph with assymetric error bars",200,10,700,500);
-   c1->SetFillColor(42);
-   c1->SetGrid();
-   c1->GetFrame()->SetFillColor(21);
-   c1->GetFrame()->SetBorderSize(12);
+  TCanvas* c1 = new TCanvas("c1","A Simple Graph with assymetric error bars",200,10,700,500);
+  c1->SetFillColor(42);
+  c1->SetGrid();
+  c1->GetFrame()->SetFillColor(21);
+  c1->GetFrame()->SetBorderSize(12);
 
-   Double_t exl[Nthresholds] ={0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
-   Double_t eyl[Nthresholds] ={0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
-   Double_t exh[Nthresholds] ={0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
-   Double_t eyh[Nthresholds] ={0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
+  Double_t exl[Nthresholds] ={0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
+  Double_t eyl[Nthresholds] ={0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
+  Double_t exh[Nthresholds] ={0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
+  Double_t eyh[Nthresholds] ={0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
 
-   TGraphAsymmErrors *gr = new TGraphAsymmErrors(Nthresholds,offlinethresholds,rates,exl,exh,eyl,eyh);
-   gr->SetTitle("TGraphAsymmErrors Example");
-   gr->SetMarkerColor(4);
-   gr->SetMarkerStyle(21);
-   gr->Draw("ALP");
+  TGraphAsymmErrors *gr = new TGraphAsymmErrors(Nthresholds,offlinethresholds,rates,exl,exh,eyl,eyh);
+  gr->SetTitle("TGraphAsymmErrors Example");
+  gr->SetMarkerColor(4);
+  gr->SetMarkerStyle(21);
+  gr->Draw("ALP");
 
-   TFile*foutput=new TFile(Form("%s_cent%d.root",outfile.Data(),centrality),"recreate");
-   foutput->cd();
-   gr->Write();
-   foutput->Close();
+  TFile*foutput=new TFile(Form("%s_cent%d.root",outfile.Data(),centrality),"recreate");
+  foutput->cd();
+  gr->Write();
+  //////// Kaya's modificiation ////////
+  rate->Write();
+  //////// Kaya's modificiation - END ////////
+  foutput->Close();
 
 
 }
@@ -172,7 +184,6 @@ int main(int argc, char **argv)
     findthes(argv[1], argv[2], argv[3], atoi(argv[4]));
     return 0;
   }else  {
-    std::cout << "Usage: \nmakeTurnOn_fromSameFile.exe <input_HiForest_file> <output_file>" << std::endl;
     return 1;
   }
 }
