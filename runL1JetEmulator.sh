36f8a339128acab1c86750b7458a21acba92d2e8
R. Alex Barbieri <luck@mit.edu>
2015-05-01 11:17:02 -0400

    add new sample to runscript

--- runL1JetEmulator.sh
+++ runL1JetEmulator.sh
@@ -19,9 +19,9 @@ set -x
   #10  twoByTwoANDzeroWallsANDsigmaSubtraction
   # };
 
-InputType=(MBData Hydjet276 Hydjet502 Hydjet502Dijet30)
-InputHiForest=("/mnt/hadoop/cms/store/user/luck/L1Emulator/minbiasForest_merged_v2/HiForest_PbPb_Data_minbias_fromSkim_v3.root" "/mnt/hadoop/cms/store/user/ginnocen/Hydjet1p8_TuneDrum_Quenched_MinBias_2760GeV/HiMinBias_Forest_26June2014/d9ab4aca1923b3220eacf8ee0d550950/*.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/HydjetMB_502TeV_740pre8_MCHI2_74_V3_HiForestAndEmulator_v5.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/PyquenUnquenched_DiJet_pt30_PbPb_5020GeV_actuallyEmbedded_HiForest.root")
-InputL1=("/mnt/hadoop/cms/store/user/men1/L1Data/HIL1DPG/MinBias/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_v2_CaloRegionEta516_CMSSW740pre7/L1NTupleMBHIFS.root" "/export/d00/scratch/luck/Hydjet1p8_2760GeV_L1UpgradeAnalyzer_GT_run1_mc_HIon_L1UpgradeAnalyzer.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/HydjetMB_502TeV_740pre8_MCHI2_74_V3_HiForestAndEmulator_v5.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/PyquenUnquenched_DiJet_pt30_PbPb_5020GeV_actuallyEmbedded_HiForest.root")
+InputType=(MBData Hydjet276 Hydjet502 Hydjet502Dijet30 Hydjet502Dijet80)
+InputHiForest=("/mnt/hadoop/cms/store/user/luck/L1Emulator/minbiasForest_merged_v2/HiForest_PbPb_Data_minbias_fromSkim_v3.root" "/mnt/hadoop/cms/store/user/ginnocen/Hydjet1p8_TuneDrum_Quenched_MinBias_2760GeV/HiMinBias_Forest_26June2014/d9ab4aca1923b3220eacf8ee0d550950/*.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/HydjetMB_502TeV_740pre8_MCHI2_74_V3_HiForestAndEmulator_v5.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/PyquenUnquenched_DiJet_pt30_PbPb_5020GeV_actuallyEmbedded_HiForest.root" "/mnt/hadoop/cms/store/user/dgulhan/HiForest_Ncoll_Dijet_pthat80_740pre8_MCHI2_74_V3_mergedx2/HiForest_Ncoll_Dijet_pthat80_740pre8_MCHI2_74_V3_merged_forest_0.root")
+InputL1=("/mnt/hadoop/cms/store/user/men1/L1Data/HIL1DPG/MinBias/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_v2_CaloRegionEta516_CMSSW740pre7/L1NTupleMBHIFS.root" "/export/d00/scratch/luck/Hydjet1p8_2760GeV_L1UpgradeAnalyzer_GT_run1_mc_HIon_L1UpgradeAnalyzer.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/HydjetMB_502TeV_740pre8_MCHI2_74_V3_HiForestAndEmulator_v5.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/PyquenUnquenched_DiJet_pt30_PbPb_5020GeV_actuallyEmbedded_HiForest.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/PyquenUnquenched_Dijet_pthat80_740pre8_MCHI2_74_V3_noHoEorFG_2x2jets.root")
 
 AlgoVariations=(nominal zeroWalls doubleSubtraction sigmaSubtraction barrelOnly oneByOne twoByTwo oneByOneAndzeroWalls oneByOneANDzeroWallsANDsigmaSubtraction twoByTwoANDzeroWalls twoByTwoANDzeroWallsANDsigmaSubtraction)
 
@@ -33,9 +33,9 @@ g++ makeTurnOn_fromSameFile.C $(root-config --cflags --libs) -Werror -Wall -Wext
 g++ plotTurnOn.C $(root-config --cflags --libs) -Werror -Wall -Wextra -O2 -o plotTurnOn.exe || exit 1
 
 
-for sampleNum in 2 3
+for sampleNum in 4
 do
-    for algo in 10
+    for algo in 9 10
     do
 	L1Output="~/scratch/EmulatorResults/${InputType[sampleNum]}_JetResults_${AlgoVariations[algo]}_2GeVBin.root"
 	HistOutput="hist_${InputType[sampleNum]}_${AlgoVariations[algo]}_2GeVBin.root"
@@ -45,7 +45,7 @@ do
 	if [[ $sampleNum -eq 0 ]]
 	then
 	    ./makeTurnOn.exe "$L1Output" "${InputHiForest[sampleNum]}" "$HistOutput" 0 0 || exit 1
-	elif [[ $sampleNum -eq 1 ]]
+	elif [[ $sampleNum -eq 1 ]] || [[ $sampleNum -eq 4 ]]
 	then
 	    ./makeTurnOn.exe "$L1Output" "${InputHiForest[sampleNum]}" "$HistOutput" 1 0 || exit 1
 	elif [[ $sampleNum -eq 2 ]] || [[ $sampleNum -eq 3 ]]
