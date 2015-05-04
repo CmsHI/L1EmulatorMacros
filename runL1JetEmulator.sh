74afca68b10e71e5108134bf4ec6aa5118d49212
R. Alex Barbieri <luck@mit.edu>
2015-05-04 11:46:29 -0400

    run everything at once

--- runL1JetEmulator.sh
+++ runL1JetEmulator.sh
@@ -17,11 +17,14 @@ set -x
   #8  oneByOneANDzeroWallsANDsigmaSubtraction,
   #9  twoByTwoANDzeroWalls,
   #10  twoByTwoANDzeroWallsANDsigmaSubtraction
-  # };
+# };
+
+BINSIZES=("" "_2GeVBin")
+BINSIZE=0
 
 InputType=(MBData Hydjet276 Hydjet502 Hydjet502Dijet30 Hydjet502Dijet80)
-InputHiForest=("/mnt/hadoop/cms/store/user/luck/L1Emulator/minbiasForest_merged_v2/HiForest_PbPb_Data_minbias_fromSkim_v3.root" "/mnt/hadoop/cms/store/user/ginnocen/Hydjet1p8_TuneDrum_Quenched_MinBias_2760GeV/HiMinBias_Forest_26June2014/d9ab4aca1923b3220eacf8ee0d550950/*.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/HydjetMB_502TeV_740pre8_MCHI2_74_V3_HiForestAndEmulator_v5.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/PyquenUnquenched_DiJet_pt30_PbPb_5020GeV_actuallyEmbedded_HiForest.root" "/mnt/hadoop/cms/store/user/dgulhan/HiForest_Ncoll_Dijet_pthat80_740pre8_MCHI2_74_V3_mergedx2/HiForest_Ncoll_Dijet_pthat80_740pre8_MCHI2_74_V3_merged_forest_0.root")
-InputL1=("/mnt/hadoop/cms/store/user/men1/L1Data/HIL1DPG/MinBias/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_v2_CaloRegionEta516_CMSSW740pre7/L1NTupleMBHIFS.root" "/export/d00/scratch/luck/Hydjet1p8_2760GeV_L1UpgradeAnalyzer_GT_run1_mc_HIon_L1UpgradeAnalyzer.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/HydjetMB_502TeV_740pre8_MCHI2_74_V3_HiForestAndEmulator_v5.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/PyquenUnquenched_DiJet_pt30_PbPb_5020GeV_actuallyEmbedded_HiForest.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/PyquenUnquenched_Dijet_pthat80_740pre8_MCHI2_74_V3_noHoEorFG_2x2jets.root")
+InputHiForest=("/mnt/hadoop/cms/store/user/luck/L1Emulator/minbiasForest_merged_v2/HiForest_PbPb_Data_minbias_fromSkim_v3.root" "/mnt/hadoop/cms/store/user/ginnocen/Hydjet1p8_TuneDrum_Quenched_MinBias_2760GeV/HiMinBias_Forest_26June2014/d9ab4aca1923b3220eacf8ee0d550950/*.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/HydjetMB_502TeV_740pre8_MCHI2_74_V3_rctconfigNoCuts_HiForestAndEmulatorAndHLT_v7.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/PyquenUnquenched_DiJet_pt30_PbPb_5020GeV_actuallyEmbedded_HiForest.root" "/mnt/hadoop/cms/store/user/dgulhan/HiForest_Ncoll_Dijet_pthat80_740pre8_MCHI2_74_V3_mergedx2/HiForest_Ncoll_Dijet_pthat80_740pre8_MCHI2_74_V3_merged_forest_0.root")
+InputL1=("/mnt/hadoop/cms/store/user/men1/L1Data/HIL1DPG/MinBias/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_v2_CaloRegionEta516_CMSSW740pre7/L1NTupleMBHIFS.root" "/export/d00/scratch/luck/Hydjet1p8_2760GeV_L1UpgradeAnalyzer_GT_run1_mc_HIon_L1UpgradeAnalyzer.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/HydjetMB_502TeV_740pre8_MCHI2_74_V3_rctconfigNoCuts_HiForestAndEmulatorAndHLT_v7.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/PyquenUnquenched_DiJet_pt30_PbPb_5020GeV_actuallyEmbedded_HiForest.root" "/mnt/hadoop/cms/store/user/luck/L1Emulator/PyquenUnquenched_Dijet_pthat80_740pre8_MCHI2_74_V3_noHoEorFG_2x2jets.root")
 
 AlgoVariations=(nominal zeroWalls doubleSubtraction sigmaSubtraction barrelOnly oneByOne twoByTwo oneByOneAndzeroWalls oneByOneANDzeroWallsANDsigmaSubtraction twoByTwoANDzeroWalls twoByTwoANDzeroWallsANDsigmaSubtraction)
 
@@ -31,15 +34,16 @@ g++ makeRateCurve.C $(root-config --cflags --libs) -Werror -Wall -Wextra -O2 -o 
 g++ makeTurnOn.C $(root-config --cflags --libs) -Werror -Wall -Wextra -O2 -o makeTurnOn.exe || exit 1
 g++ makeTurnOn_fromSameFile.C $(root-config --cflags --libs) -Werror -Wall -Wextra -O2 -o makeTurnOn_fromSameFile.exe || exit 1
 g++ plotTurnOn.C $(root-config --cflags --libs) -Werror -Wall -Wextra -O2 -o plotTurnOn.exe || exit 1
-
+g++ findthes.C $(root-config --cflags --libs) -Werror -Wall -Wextra -O2 -o findthes.exe || exit 1
 
 for sampleNum in 2 3 4
 do
     for algo in 9 10
     do
-	L1Output="~/scratch/EmulatorResults/${InputType[sampleNum]}_JetResults_${AlgoVariations[algo]}.root"
-	HistOutput="hist_${InputType[sampleNum]}_${AlgoVariations[algo]}.root"
-	PlotOutputTag="${InputType[sampleNum]}_${AlgoVariations[algo]}"
+	L1Output="~/scratch/EmulatorResults/${InputType[sampleNum]}_JetResults_${AlgoVariations[algo]}${BINSIZES[BINSIZE]}.root"
+	HistOutput="hist_${InputType[sampleNum]}_${AlgoVariations[algo]}${BINSIZES[BINSIZE]}.root"
+	PlotOutputTag="${InputType[sampleNum]}_${AlgoVariations[algo]}${BINSIZES[BINSIZE]}"
+	THRESHOUTFILE="rate_${sample}_${algo}${BINSIZES[BINSIZE]}"
 	./L1JetEmulator.exe "${InputL1[sampleNum]}" "$L1Output" $algo || exit 1
 	./makeRateCurve.exe "$L1Output" || exit 1
 	if [[ $sampleNum -eq 0 ]]
@@ -53,5 +57,15 @@ do
 	   ./makeTurnOn_fromSameFile.exe "$L1Output" "${InputHiForest[sampleNum]}" "$HistOutput" 1 0 || exit 1
 	fi
 	./plotTurnOn.exe "$HistOutput" "$PlotOutputTag" || exit 1
+	./findthes.exe "$L1Ouput" "$HistOutput" "$THRESHOUTFILE" 0 || exit 1
+    done
+done
+
+# run this last just so the output is cleaner.
+for sampleNum in 2 3 4
+do
+    for algo in 9 10
+    do
+	./findthes.exe "$L1Ouput" "$HistOutput" "$THRESHOUTFILE" 0 || exit 1
     done
 done
